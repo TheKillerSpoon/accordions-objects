507afc952e3de4786e98bc52df243aa890597a90
TheKillerSpoon <75366115+TheKillerSpoon@users.noreply.github.com>
2024-08-12 10:54:25 +0200

    EWTSGRDXGRSDRZDGR

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,66 @@
 
 - for at få dem udskrevet, skal der forEach'es over array'et med objekter
 - i forEach'en udskrives html'en hver accordion (inde i ``)
+*/
+let textArray =[
+    {
+        title: "FUCK",
+        bodytext: "hmmm"
+    },
+    {
+        title: "MIT",
+        bodytext: "aha"
+    },
+    {
+        title: "LIV",
+        bodytext: "helt sikkert"
+    }
+];
+
+const accordions = document.querySelector(".container");
+
+
+textArray.forEach(e =>{
+    accordions.innerHTML += `
+        <div class="accordion">${e.title}</p>
+            <div class="panel">
+                <p>${e.bodytext}</p>
+            </div>
+        </div>
+    `
+})
 
 
 
+const accOutput = document.querySelectorAll(".accordion");
 
+accOutput.forEach(element =>{
+    element.addEventListener("click", (e)=>{
+        e.currentTarget.classList.toggle("active")
+    })
+})
+
+
+
+/*
 ----- Funktionalitet (åbn & lukke): -----
 - Hver accordion har class'en .accordion. gem dem ned i en variabel ( brug querySelectorAll), og foreach over dem, så hver enkel kan trykkes på
     - brug e.currentTarget
 - husk 'classList.toggle' istedet for 'add'
 
 */
+/*
+const accOutput = document.querySelectorAll(".accordion");
+
+accOutput.forEach(element =>{
+    element.addEventListener("click", (e)=>{
+        e.currentTarget.classList.toggle("active")
+    })
+})
+*/
+
+
+
+
+
 
